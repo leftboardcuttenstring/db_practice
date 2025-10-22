bce852e95f0063ecbcae555fde28eb3bfeae1a52
mike <michael.a.strangewood@gmail.com>
2025-10-22 12:41:50 +0300

    Added a query of inserting the data into a table

---
+++ db_create/instering_data.sql
@@ -0,0 +1,15 @@
+insert into dbo.applicants (applicant_name, applicant_surname, applicant_patronymic, dob, faculty_for_admission) values ('Иван', 'Иванов', 'Иванович', '2023-01-01', 'Программирование');
+
+/*
+DISCRIPTION:
+
+insert into dbo.applicants (applicant_name,                                     - dbo.applicants - table to what you want to insert the data, applicant_name and etc. are columns
+							applicant_surname,
+							applicant_patronymic, 
+							dob, 
+							faculty_for_admission) values ('Иван',              - values - is a keyword for inserting the data what's described after that to the columns what're described before
+														   'Иванов', 
+														   'Иванович', 
+														   '2023-01-01', 
+														   'Программирование'); -
+*/
