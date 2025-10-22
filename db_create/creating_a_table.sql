919a8cdbb3ad9ba3b96e49e84f90e635f1852526
mike <michael.a.strangewood@gmail.com>
2025-10-22 10:36:29 +0300

    New SQL-query for creating a table for database (with discription)

---
+++ db_create/creating_a_table.sql
@@ -0,0 +1,19 @@
+create table applicants (
+	applicant_name varchar(50),
+	applicant_surname varchar(50),
+	applicant_patronymic varchar(50),
+	dob date,
+	faculty_for_admission varchar(50)
+)
+
+/*
+ОПИСАНИЕ:
+
+create table applicants (             - applicants - название таблицы (можете подставить свое, если вам нужно другое название)
+	applicant_name varchar(50),       - applicant_name - название столбца, varchar(60) тип данных в столбце applicant_name
+	applicant_surname varchar(50),
+	applicant_patronymic varchar(50),
+	dob date,
+	faculty_for_admission varchar(50)
+)
+*/
