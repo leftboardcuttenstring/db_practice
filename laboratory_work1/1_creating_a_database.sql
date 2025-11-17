9f6eeba6247041d967b5f56f2045cabd62d2d372
mike <michael.a.strangewood@gmail.com>
2025-11-17 17:08:13 +0300

    The structure of query file names has been changed

---
+++ laboratory_work1/1_creating_a_database.sql
@@ -0,0 +1,10 @@
+create database my_new_batabase;
+
+/*
+DISCRIPTION:
+
+create database my_new_batabase;:
+	create          - that is keyword for creating a database
+	database        - keyword too
+	my_new_database - database's name (you can insert another value)
+*/
